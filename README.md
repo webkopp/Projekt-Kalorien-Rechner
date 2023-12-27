6b8bed2ed2a031af57634c9e7e9281f35e490c0b
webkopp <nakopps@googlemail.com>
2023-12-27 15:22:38 +0100

    projekt-015-kalorien-rechner

---
+++ README.md
@@ -0,0 +1,34 @@
+Kalorien-Rechner!!!
+
+Beschreibung
+Dieses JavaScript-Projekt wurde im Rahmen eines Coding-Bootcamps bei supercode.de erstellt. Es handelt sich um einen Kalorienrechner, bei der man nach Eingabe der Körperdaten und dem Geschlecht, den idealen Kalorienwert/Tag angezeigt bekommt.
+
+Die Anwendung bietet folgende Möglichkeiten:
+- Die User*innen können über Radio-Button ihr Geschlecht auswählen.
+- Nun können über Input-Felder Gewicht, Körpergröße und Alter eingegeben werden.
+- Über den Button "Kalorienbedarf" kann die Berechnung gestartet werden.
+- Der ermittelte, ideale Tagesbedarf an Kalorien wird nun unter dem Button angezeigt.
+
+Werkzeuge/Technologien
+HTML5
+CSS3
+JavaScript
+
+GitHub Live-Vorschau
+Du findest eine Live Vorschau auf GitHub: 
+
+GitHub-Repository
+Du findest das gesamte Projekt auf GitHub: 
+
+Aufgaben
+- Programmiere einen Kalorienrechner:
+- Zuerst musst du dafür den Grundumsatz berechnen. Der Grundumsatz wird anhand der Harris-Benedict-Formel berechnet.
+- Grundumsatz bei Männern
+(Kalorien je Tag)  66.47 + (13.7 Körpergewicht in kg) + (5 Körpergröße in cm) – (6.8 Alter in Jahren) = Grundumsatz
+- Grundumsatz bei Frauen
+(Kalorien je Tag)  655.1 + (9.6 Körpergewicht in kg) + (1.8 Körpergröße in cm) – (4.7 Alter in Jahren) = Grundumsatz
+- Achtung: Das Ergebnis wird in Kilokalorien ausgegeben!
+
+Anmerkungen
+Dieses Projekt wurde im Rahmen eines Coding-Bootcamps bei supercode.de erstellt.
+Die Anwendung ermöglicht das Ermitteln des täglichen Kalorienbedarfs durch eingebeben und berechnen der Grundumsatz-Daten wie Geschlecht, Gewicht, Körpergröße und Alter.
---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+// Programmiere einen Kalorienrechner.
+// - Zuerst musst du dafür den Grundumsatz berechnen. Der Grundumsatz wird anhand der Harris-Benedict-Formel berechnet.
+// - Grundumsatz bei Männern
+// (Kalorien je Tag)  66.47 + (13.7 Körpergewicht in kg) + (5Körpergröße in cm) – (6.8 Alter in Jahren) = Grundumsatz
+// - Grundumsatz bei Frauen
+// (Kalorien je Tag)  655.1 + (9.6 Körpergewicht in kg) + (1.8 Körpergröße in cm) – (4.7 Alter in Jahren) = Grundumsatz
+// - Achtung: Das Ergebnis wird in Kilokalorien ausgegeben!
+
+// # ☝🏼 Hinweis
+// - Um den Gesamtumsatz auszurechnen, sollte der PAL-Faktor (PAL=Physical Activity Level) mit dem Grundumsatz multipliziert werden.
+
+
+const kalorienrechner = () => {
+    let geschlecht = document.querySelector("#geschlecht").value
+    let gewicht = document.querySelector("#gewicht").value
+    let groesse = document.querySelector("#groesse").value
+    let alter = document.querySelector("#alter").value
+    let palFaktor = document.querySelector("#palFaktorOptions").value
+
+    function berechneGrundumsatz(geschlecht, gewicht, groesse, alter) {
+        let grundumsatz = 0
+
+        switch(geschlecht) {
+            case "männlich":
+                grundumsatz = 66.47 + (13.7 * gewicht) + (5 * groesse) - (6.8 * alter)
+                break
+            case "weiblich":
+                grundumsatz = 655.1 + (9.6 * gewicht) + (1.8 * groesse) - (4.7 * alter)
+                break
+            default:
+                console.log("ungültiges Geschlecht")
+        }
+        return grundumsatz
+    }
+
+    function berechneGesamtumsatz(grundumsatz, palFaktor) {
+        return grundumsatz * palFaktor
+    }
+}
+
+ergebnis.innerHTML = "Dein idealer Kalorienbedarf nach Harris-Benedict-Formel ist " + Math.round(kcal) + " kcal pro Tag."
